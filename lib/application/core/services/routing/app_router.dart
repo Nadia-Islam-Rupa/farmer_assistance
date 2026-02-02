21b7eb941ef222ad82450e996150a8982a0d7f89
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-02-02 22:49:36 +0600

    feat: Enhance routing logic with additional authentication pages and restore auth state listener

--- lib/application/core/services/routing/app_router.dart
+++ lib/application/core/services/routing/app_router.dart
@@ -8,30 +8,41 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../pages/auth/create_account_page/create_account.dart';
+import '../../../pages/auth/forget_password_page/forgot_pass.dart';
+
 class AppRouter {
   static final rootNavigatorKey = GlobalKey<NavigatorState>();
   static final GoRouter _router = GoRouter(
     debugLogDiagnostics: true,
     navigatorKey: rootNavigatorKey,
-    // // 🔹 Listen for auth changes
-    // refreshListenable: GoRouterRefreshStream(
-    //   FirebaseAuth.instance.authStateChanges(),
-    // ),
-    // // 🔹 Redirect logic
-    // redirect: (context, state) {
-    //   final user = FirebaseAuth.instance.currentUser;
-    //   final isLoggingIn = state.fullPath == PAGES.loginDash.screenPath;
-    //
-    //   if (user == null) {
-    //     // Not logged in → force to login page unless already there
-    //     return isLoggingIn ? null : PAGES.loginDash.screenPath;
-    //   }
-    //
-    //   // Logged in but tries to access login page → send to bottom page
-    //   if (isLoggingIn) return PAGES.homePage.screenPath;
-    //
-    //   return null; // No redirection
-    // },
+    // 🔹 Listen for auth changes
+    refreshListenable: GoRouterRefreshStream(
+      FirebaseAuth.instance.authStateChanges(),
+    ),
+    // 🔹 Redirect logic
+    redirect: (context, state) {
+      final user = FirebaseAuth.instance.currentUser;
+
+      final authRoutes = [
+        PAGES.loginDash.screenPath,
+        PAGES.loginPage.screenPath,
+        PAGES.createAccountPage.screenPath,
+        PAGES.forgetPage.screenPath,
+      ];
+      // final isLoggingIn = state.fullPath == PAGES.loginDash.screenPath;
+      final isLoggingIn = authRoutes.contains(state.fullPath);
+
+      if (user == null) {
+        // Not logged in → force to login page unless already there
+        return isLoggingIn ? null : PAGES.loginDash.screenPath;
+      }
+
+      // Logged in but tries to access login page → send to bottom page
+      if (isLoggingIn) return PAGES.homePage.screenPath;
+
+      return null; // No redirection
+    },
     routes: [
       GoRoute(
         path: PAGES.loginDash.screenPath,
@@ -40,12 +51,23 @@ class AppRouter {
       ),
       GoRoute(
         path: PAGES.loginPage.screenPath,
+        name: PAGES.loginPage.screenName,
         builder: (context, state) => LoginPage(),
       ),
       GoRoute(
         path: PAGES.homePage.screenPath,
         name: PAGES.homePage.screenName,
         builder: (context, state) => HomePage(),
+      ),
+      GoRoute(
+        path: PAGES.createAccountPage.screenPath,
+        name: PAGES.createAccountPage.screenName,
+        builder: (context, state) => CreateAccountPage(),
+      ),
+      GoRoute(
+        path: PAGES.forgetPage.screenPath,
+        name: PAGES.forgetPage.screenName,
+        builder: (context, state) => ForgotPasswordPage(),
       ),
     ],
   );
