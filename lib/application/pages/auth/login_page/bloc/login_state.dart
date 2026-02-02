08cfdcb56499f43d1dfb19b0f623993c2ac78f4c
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-02-02 18:39:27 +0600

    feat: Add LoginState for BLoC management
    - Create `LoginState` using `freezed` to handle different states.
    - Define initial, loading, success, and error states.
    - Include states for sign-out process: `signOut`, `errorSignOut`, and `signOutSuccess`.

---
+++ lib/application/pages/auth/login_page/bloc/login_state.dart
@@ -0,0 +1,13 @@
+part of 'login_bloc.dart';
+
+@freezed
+class LoginState with _$LoginState {
+  const factory LoginState.initial() = _Initial;
+  const factory LoginState.loading() = LoadingLoginState;
+  const factory LoginState.success() = SuccessLoginState;
+  const factory LoginState.error({required String message}) = ErrorLoginState;
+  const factory LoginState.signOut() = SignOutLoginState;
+  const factory LoginState.errorSignOut({required String message}) =
+      ErrorSignOut;
+  const factory LoginState.signOutSuccess() = SignOutSuccess;
+}
