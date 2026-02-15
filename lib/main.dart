ccf4703f568c987c0d0ccb4cae66432afadf2488
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-02-16 00:40:49 +0600

    Replace Firebase initialization with Supabase in `main.dart`
    - Remove Firebase core and configuration imports
    - Initialize Supabase with project URL and anonymous key
    - Clean up initialization logic in the `main` function

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:farmer_assistance/application/core/services/routing/app_router.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'di/di.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await configureDependencies();
 
+  await Supabase.initialize(
+    url: "https://rdqpssnvyywzjiqrokfv.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkcXBzc252eXl3emppcXJva2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0Mzg3NDIsImV4cCI6MjA4NjAxNDc0Mn0.3qjJlUXak0Jevxhp4OUdjXHYV1jJhjd687YHo8OlJQU",
+  );
+
+  await configureDependencies();
   runApp(ProviderScope(child: const MyApp()));
 }
 
