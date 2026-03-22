510bf924d4e05c50c72bfcedfc5397c363ecd56b
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-03-22 21:33:04 +0600

    Refactor showSnackBar method for improved readability and add getGreeting method

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Utils {
-  static void showSnackBar(BuildContext context, String message, Color backgroundColor) {
+  static void showSnackBar(
+    BuildContext context,
+    String message,
+    Color backgroundColor,
+  ) {
     final messenger = ScaffoldMessenger.of(context);
     messenger.hideCurrentSnackBar(); // Hide previous snackbar if any
 
@@ -13,5 +17,17 @@ class Utils {
         backgroundColor: backgroundColor,
       ),
     );
+  }
+
+  static String getGreeting() {
+    final hour = DateTime.now().hour;
+
+    if (hour < 12) {
+      return 'Good Morning!';
+    } else if (hour < 17) {
+      return 'Good Afternoon!';
+    } else {
+      return 'Good Evening!';
+    }
   }
 }
