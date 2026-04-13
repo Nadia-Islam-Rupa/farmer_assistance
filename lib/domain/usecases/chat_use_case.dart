19f76f12d917456a3e9aae2058996d691842203f
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-13 23:26:48 +0600

    Add a newline for formatting in `chat_use_case.dart`
    - Improve code readability by adding a vertical space between imports in `lib/domain/usecases/chat_use_case.dart`.

--- lib/domain/usecases/chat_use_case.dart
+++ lib/domain/usecases/chat_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
+
 import '../failures/failures.dart';
 import '../models/chat_models.dart';
 import '../repositories/chat_repository.dart';
