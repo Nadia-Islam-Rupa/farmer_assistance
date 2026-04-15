f68aee46e33af9ba9314e8982525ab22e8558f92
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-16 00:25:05 +0600

    Add `updateProfileInfo` event to `ProfileEvent`
    - Update `lib/application/pages/profile/bloc/profile_event.dart` to include the `updateProfileInfo` factory.
    - The new event accepts an optional `File` for the profile picture and a required `String` for the name.

--- lib/application/pages/profile/bloc/profile_event.dart
+++ lib/application/pages/profile/bloc/profile_event.dart
@@ -3,4 +3,6 @@ part of 'profile_bloc.dart';
 @freezed
 class ProfileEvent with _$ProfileEvent {
   const factory ProfileEvent.fetchProfileInfo() = _FetchProfileInfo;
+  const factory ProfileEvent.updateProfileInfo({required File? file, required String name}) = _UpdateProfileInfo;
+
 }
