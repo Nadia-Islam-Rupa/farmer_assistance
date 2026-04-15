99f6806a22282e91384c77570ecd85b85514c836
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-16 00:25:15 +0600

    Implement `_UpdateProfileInfo` event handler in `ProfileBloc`
    - Add logic to handle profile updates, including support for updating the user's name and profile image file.
    - Implement validation to ensure at least one field is provided before proceeding.
    - Handle state transitions for `loading`, `updatedProfileInfo`, and `error` states based on the `ProfileUseCase` response.
    - Add `dart:io` import to support file handling.

--- lib/application/pages/profile/bloc/profile_bloc.dart
+++ lib/application/pages/profile/bloc/profile_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:farmer_assistance/domain/failures/failures.dart';
 import 'package:farmer_assistance/domain/usecases/profile_use_case.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -27,6 +29,38 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
             emit(const ProfileState.error(message: 'Something went wrong'));
           }
         }, (user) => emit(ProfileState.loaded(userModel: user)));
+      } catch (e) {
+        emit(const ProfileState.error(message: 'Something went wrong'));
+      }
+    });
+
+    on<_UpdateProfileInfo>((event, emit) async {
+      emit(const ProfileState.loading());
+      try {
+        if (event.file == null && event.name.isEmpty) {
+          print("object");
+          emit(
+            const ProfileState.error(message: 'Please fill at list one fields'),
+          );
+          return;
+        }
+        final response = await _profileUseCase.updateProfile(
+          event.file,
+          event.name,
+        );
+
+        response.fold(
+          (l) {
+            if (l is GeneralFailure) {
+              emit(ProfileState.error(message: l.message));
+            } else {
+              emit(const ProfileState.error(message: 'Something went wrong'));
+            }
+          },
+          (r) {
+            emit(const ProfileState.updatedProfileInfo());
+          },
+        );
       } catch (e) {
         emit(const ProfileState.error(message: 'Something went wrong'));
       }
