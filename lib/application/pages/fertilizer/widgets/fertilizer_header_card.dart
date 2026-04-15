2fd154b2e6952a4773238842559fe5eea37a29d2
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-16 01:56:43 +0600

    Refactor FertilizerHeaderCard to use AppTheme colors for consistency

--- lib/application/pages/fertilizer/widgets/fertilizer_header_card.dart
+++ lib/application/pages/fertilizer/widgets/fertilizer_header_card.dart
@@ -1,4 +1,4 @@
-import 'package:farmer_assistance/application/pages/water_prediction/water_prediction_theme.dart';
+import 'package:farmer_assistance/application/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class FertilizerHeaderCard extends StatelessWidget {
@@ -27,7 +27,7 @@ class FertilizerHeaderCard extends StatelessWidget {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              WaterPredictionTheme.primaryTeal.withValues(alpha: 0.08),
+              AppTheme.primaryTeal.withValues(alpha: 0.08),
               Colors.white,
             ],
           ),
@@ -37,12 +37,12 @@ class FertilizerHeaderCard extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(14),
               decoration: BoxDecoration(
-                color: WaterPredictionTheme.primaryTeal.withValues(alpha: 0.15),
+                color: AppTheme.primaryTeal.withValues(alpha: 0.15),
                 borderRadius: BorderRadius.circular(14),
               ),
               child: const Icon(
                 Icons.science_outlined,
-                color: WaterPredictionTheme.deepTeal,
+                color: AppTheme.deepTeal,
                 size: 32,
               ),
             ),
@@ -56,7 +56,7 @@ class FertilizerHeaderCard extends StatelessWidget {
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: WaterPredictionTheme.deepTeal,
+                      color: AppTheme.deepTeal,
                     ),
                   ),
                   const SizedBox(height: 6),
@@ -76,7 +76,7 @@ class FertilizerHeaderCard extends StatelessWidget {
                         const Icon(
                           Icons.cloud_done_outlined,
                           size: 14,
-                          color: WaterPredictionTheme.primaryTeal,
+                          color: AppTheme.primaryTeal,
                         ),
                         const SizedBox(width: 6),
                         Expanded(
@@ -84,7 +84,7 @@ class FertilizerHeaderCard extends StatelessWidget {
                             weatherSourceLabel,
                             style: const TextStyle(
                               fontSize: 11,
-                              color: WaterPredictionTheme.primaryTeal,
+                              color: AppTheme.primaryTeal,
                               fontWeight: FontWeight.w600,
                             ),
                           ),
