60e7b2421fc83b8edf60a4ab9e0f9b48d7a8514c
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-21 22:14:21 +0600

    Enhance yield estimation repository to include user data insertion in Supabase

--- lib/data/repositories/yield_estimation_repositories_iml.dart
+++ lib/data/repositories/yield_estimation_repositories_iml.dart
@@ -5,6 +5,7 @@ import 'package:farmer_assistance/domain/failures/failures.dart';
 import 'package:farmer_assistance/domain/models/yield_estimation_request_model.dart';
 import 'package:farmer_assistance/domain/models/yield_estimation_response_model.dart';
 import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../domain/failures/dio_error.dart';
 import '../../domain/repositories/yield_estimation_repository.dart';
@@ -12,8 +13,9 @@ import '../../domain/repositories/yield_estimation_repository.dart';
 @Injectable(as: YieldEstimationRepository)
 class YieldEstimationRepositoriesIml extends YieldEstimationRepository {
   final ApiService _apiService;
+  final SupabaseClient _supabaseClient;
 
-  YieldEstimationRepositoriesIml(this._apiService);
+  YieldEstimationRepositoriesIml(this._apiService, this._supabaseClient);
 
   @override
   Future<Either<Failures, YieldEstimationResponseModel>> estimateYield(
@@ -23,6 +25,23 @@ class YieldEstimationRepositoriesIml extends YieldEstimationRepository {
       final response = await _apiService.estimateYield(
         yieldEstimationRequestData: requestData,
       );
+
+      final userId = _supabaseClient.auth.currentUser?.id;
+      if (userId != null) {
+        await _supabaseClient.from('yield_estimations').insert({
+          'user_id': userId,
+          'item': requestData.item,
+          'area': requestData.area,
+          'rainfall': requestData.rainfall,
+          'temperature': requestData.temperature,
+          'pesticides': requestData.pesticides,
+          'predicted_yield': response.predictedYield,
+          'yield_unit': response.yieldUnit,
+          'confidence': response.confidence,
+          'feature_importance': response.featureImportance?.toJson(),
+        });
+      }
+
       return right(response);
     } catch (e) {
       if (e is DioException) {
