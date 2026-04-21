56fa896d28b880a9d7e64223c629188ecc5b6fe9
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-21 22:14:01 +0600

    Enhance crop recommendation repository to include user data insertion in Supabase

--- lib/data/repositories/crop_recommendation_repositories_iml.dart
+++ lib/data/repositories/crop_recommendation_repositories_iml.dart
@@ -5,6 +5,7 @@ import 'package:farmer_assistance/domain/models/Crop_recommendation_request_mode
 import 'package:farmer_assistance/domain/models/Crop_recommendation_response_model.dart';
 import 'package:farmer_assistance/domain/repositories/crop_recommendation_repository.dart';
 import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../domain/failures/dio_error.dart';
 import '../datasources/remote_datasource/api_service.dart.dart';
@@ -12,7 +13,9 @@ import '../datasources/remote_datasource/api_service.dart.dart';
 @Injectable(as: CropRecommendationRepository)
 class CropRecommendationRepositoriesIml extends CropRecommendationRepository {
   final ApiService _apiService;
-  CropRecommendationRepositoriesIml(this._apiService);
+  final SupabaseClient _supabaseClient;
+
+  CropRecommendationRepositoriesIml(this._apiService, this._supabaseClient);
 
   @override
   Future<Either<Failures, CropRecommendationResponseModel>>
@@ -23,6 +26,23 @@ class CropRecommendationRepositoriesIml extends CropRecommendationRepository {
       final response = await _apiService.getCropRecommendation(
         cropRecommendationRequestModel: cropRecommendationRequestModel,
       );
+
+      final userId = _supabaseClient.auth.currentUser?.id;
+      if (userId != null) {
+        await _supabaseClient.from('crop_recommendations').insert({
+          'user_id': userId,
+          'nitrogen': cropRecommendationRequestModel.nitrogen,
+          'phosphorus': cropRecommendationRequestModel.phosphorus,
+          'potassium': cropRecommendationRequestModel.potassium,
+          'temperature': cropRecommendationRequestModel.temperature,
+          'humidity': cropRecommendationRequestModel.humidity,
+          'ph': cropRecommendationRequestModel.ph,
+          'rainfall': cropRecommendationRequestModel.rainfall,
+          'crop': response.crop,
+          'confidence': response.confidence,
+        });
+      }
+
       return right(response);
     } catch (e) {
       if (e is DioException) {
