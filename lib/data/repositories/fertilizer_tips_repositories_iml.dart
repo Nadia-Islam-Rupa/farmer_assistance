ff49eebcb44b3f342e74a005d6bc94d3ae92cae9
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-04-21 22:14:05 +0600

    Add user data insertion to fertilizer tips repository

--- lib/data/repositories/fertilizer_tips_repositories_iml.dart
+++ lib/data/repositories/fertilizer_tips_repositories_iml.dart
@@ -5,6 +5,7 @@ import 'package:farmer_assistance/domain/failures/failures.dart';
 import 'package:farmer_assistance/domain/models/fertilizer_tips_request_model.dart';
 import 'package:farmer_assistance/domain/models/fertilizer_tips_response_model.dart';
 import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../domain/failures/dio_error.dart';
 import '../../domain/repositories/fertilizer_tips_repository.dart';
@@ -12,7 +13,9 @@ import '../../domain/repositories/fertilizer_tips_repository.dart';
 @Injectable(as: FertilizerTipsRepository)
 class FertilizerTipsRepositoriesIml extends FertilizerTipsRepository {
   final ApiService _apiService;
-  FertilizerTipsRepositoriesIml(this._apiService);
+  final SupabaseClient _supabaseClient;
+
+  FertilizerTipsRepositoriesIml(this._apiService, this._supabaseClient);
 
   @override
   Future<Either<Failures, FertilizerTipsResponseModel>> fetchFertilizerTips(
@@ -22,6 +25,25 @@ class FertilizerTipsRepositoriesIml extends FertilizerTipsRepository {
       final response = await _apiService.fertilizerTips(
         fertilizerTipsRequestData: fertilizerTipsRequestData,
       );
+
+      final userId = _supabaseClient.auth.currentUser?.id;
+      if (userId != null) {
+        await _supabaseClient.from('fertilizer_tips').insert({
+          'user_id': userId,
+          'crop': fertilizerTipsRequestData.crop,
+          'soil_color': fertilizerTipsRequestData.soilColor,
+          'temperature': fertilizerTipsRequestData.temperature,
+          'nitrogen': fertilizerTipsRequestData.nitrogen,
+          'potassium': fertilizerTipsRequestData.potassium,
+          'phosphorus': fertilizerTipsRequestData.phosphorus,
+          'rainfall': fertilizerTipsRequestData.rainfall,
+          'ph': fertilizerTipsRequestData.ph,
+          'fertilizer': response.fertilizer,
+          'confidence': response.confidence,
+          'feature_importance': response.featureImportance?.toJson(),
+        });
+      }
+
       return right(response);
     } catch (e) {
       if (e is DioException) {
